d2c7d08c1e39bad70d2838d7313a0a10073ff968
Bogdan Hristinov Kyuchukov <kyuchukov@cip.ifi.lmu.de>
2023-07-01 10:29:15 +0200

    Response helper funcs added

--- internal/common/network/response.go
+++ internal/common/network/response.go
@@ -1,1 +1,24 @@
 package network
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jwo_auth/internal/common/logger"
+)
+
+func RespondWithResponseModel(model any, resWriter http.ResponseWriter) {
+	RespondWithJsonWithCustomStatusCode(model, resWriter, http.StatusOK)
+}
+
+func RespondWithJsonWithCustomStatusCode(data any, resWriter http.ResponseWriter, statusCode int) {
+	resWriter.Header().Set("Content-Type", "application/json")
+	resWriter.WriteHeader(statusCode)
+
+	jsonString, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	logger.Info(string(jsonString))
+	resWriter.Write(jsonString)
+}
