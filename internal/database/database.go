71ba637828f41c9fe0b5d72cc6ef512b47d50241
Bogdan Hristinov Kyuchukov <kyuchukov@cip.ifi.lmu.de>
2023-07-01 13:09:35 +0200

    Creating db connection works

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,4 +1,18 @@
 package database
 
-type Database interface {
+import (
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+func InitializeDatabaseConnection() *Database {
+	session, err := gorm.Open(sqlite.Open("sqlite.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return &Database{session}
 }
